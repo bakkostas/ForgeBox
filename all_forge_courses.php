78580e7449213b08e7639fecb5ffaa62037e3815
costas <kostas.bakoulias@gmail.com>
2015-02-12 15:07:26 +0200

    ok

--- all_forge_courses.php
+++ all_forge_courses.php
@@ -1,7 +1,7 @@
  <?php 
 		 include "header.php"; 
 
-		$query_course = "SELECT tbl_courses.id, tbl_courses.title, tbl_courses.author, tbl_courses.create_date, tbl_courses.sdescription FROM tbl_courses WHERE tbl_courses.active=1 AND tbl_courses.course_item_id=1 ORDER BY tbl_courses.create_date DESC LIMIT 10";		
+		$query_course = "SELECT tbl_courses.id, tbl_courses.title, tbl_courses.author, tbl_courses.create_date, tbl_courses.sdescription, tbl_courses.publisher FROM tbl_courses WHERE tbl_courses.active=1 AND tbl_courses.course_item_id=1 ORDER BY tbl_courses.create_date DESC LIMIT 10";		
 		$result_course = $connection->query($query_course);
 		
 		$url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
@@ -24,29 +24,29 @@
 			$author[$count_rec]=$row[2];
 			$create_date[$count_rec]=$row[3];
 			$sdescription[$count_rec]=$row[4];
+			$publisher[$count_rec]=$row[5];
 			$count_rec++;
 		}
  ?>
 	
 	<div class="container">
 		<h1>See All Forgebox Course</h1>
-		
-			
+			<a href="all_forge_courses_to_pdf.php">Export to PDF</a>
 			<?php 				
 				for($i=0;$i<$count_rec;$i++) {
+					
 				?>
-				<div class="row">
-					<?php echo "<div style=\"padding-left:30px;\"><h3>".$title[$i]."</h3></center></div>"; ?>
-					<div class="col-md-1">
+				<div class="row">					
+					<div class="col-md-2">
 						<img src="images/course_smallico.PNG"/>
 					</div>
-					<div class="col-md-8">						
-						<?php echo "<p>".$sdescription[$i]."</p>"; ?>
-					</div>
-					<div class="col-md-3">
-						<?php echo "<p><i class=\"fa fa-user\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Author\"></i>&nbsp;&nbsp;".$author[$i]."</p>"; ?><br />
-						<?php echo "<p><i class=\"fa fa-calendar\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Created Date\"></i>&nbsp;&nbsp;".date('d-m-Y', strtotime($create_date[$i]))."</p>"; ?><br />
-						<a href="http://localhost/forgebox/preview_course.php?course_id=<?php echo $id; ?>">See More</a>
+					<div class="col-md-10">
+						<div style="padding-left:0px;"><h3><a href="preview_course.php?course_id=<?php echo $id[$i]; ?>"><?php echo $title[$i]; ?></a></h3></div>
+						<?php echo "<p>".$sdescription[$i]."</p>"; ?>	
+						<?php echo "<div style=\"float:left; padding-right:10px; width:auto;\"><i class=\"fa fa-university \" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Publisher\"></i>&nbsp;&nbsp;".$publisher[$i]."</div>"; ?>
+						<?php echo "<div style=\"float:left; padding-right:10px; width:auto;\"><i class=\"fa fa-user\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Author\"></i>&nbsp;&nbsp;".$author[$i]."</div>"; ?>
+						<?php echo "<div style=\"float:left; padding-right:10px; width:auto;\"><i class=\"fa fa-calendar\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Created Date\"></i>&nbsp;&nbsp;".date('d-m-Y', strtotime($create_date[$i]))."</div>"; ?>
+						<div style="float:right; padding-right:10px; width:auto;"><a class="btn btn-default" href="http://localhost/forgebox/preview_course.php?course_id=<?php echo $id[$i]; ?>">See More</a></div>
 					</div>
 				</div>
 				<hr />
@@ -60,4 +60,5 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 </script>
+
 <?php include "footer.php"; ?>
