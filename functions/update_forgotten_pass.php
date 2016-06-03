e746a61f687eeb501ef68addb524069c93d51e1d
kbakoulias <kostas.bakoulias@gmail.com>
2016-06-03 16:48:48 +0300

    ok

--- functions/update_forgotten_pass.php
+++ functions/update_forgotten_pass.php
@@ -9,7 +9,7 @@
 	{
 		if(!empty($_POST['email'])){
 			
-			$query_select_uid = "SELECT id FROM tbl_users WHERE email_user=".$_POST['mail'].' AND active_user='.$_POST['actnum'];
+			$query_select_uid = "SELECT user_id FROM tbl_users WHERE email_user='".$_POST['mail']."' AND password_user='".$_POST['actnum']."'";
 			$result_select_uid = $connection->query($query_select_uid);
 			$uid =0;
 			while($row = $result_select_uid->fetch_array())
@@ -20,7 +20,7 @@
 			if($uid>0){
 				
 				
-				$$query_update_uid = "UPDATE tbl_users SET password_user=".$_POST['newpass']." WHERE id_user=".$uid;
+				$$query_update_uid = "UPDATE tbl_users SET password_user='".$_POST['newpass']."' WHERE id_user=".$uid;
 				$result_update_uid = $connection->query($query_update_uid);
 
 				
