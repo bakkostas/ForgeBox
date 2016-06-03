055986979f5a2d0de4de0bc614df9e91ef60f6ce
kbakoulias <kostas.bakoulias@gmail.com>
2016-06-03 17:14:21 +0300

    ok

--- forgot_my_pass.php
+++ forgot_my_pass.php
@@ -176,12 +176,10 @@ else{
 			
 			$.ajax({
 				type: "POST",
-				url: "functions/update_forgotten_pass,php",
+				url: "functions/update_forgotten_pass.php",
 				data: formChPass,
 				dataType: "json",
 				success: function(msg){				
-						//	hideshow('loading',0);	
-						
 						alert(msg.txt);
 						$("#notificatio_msg").html("");
 						$("#notificatio_msg").append("<span style=\"color:red;\">"+msg.txt+"</span>");
