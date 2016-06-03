512c5c051ac6299d825bd8151b2e3618fe8694f9
kbakoulias <kostas.bakoulias@gmail.com>
2016-06-03 18:01:46 +0300

    ok

--- functions/forgot_pass.php
+++ functions/forgot_pass.php
@@ -42,7 +42,7 @@
 				$headers = "MIME-Version: 1.0" . "\r\n";
 				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";				
 				$headers .= 'To: '. $to . "\r\n";
-				$headers .= 'From: Forgebox.eu <'. $myemail . "> \r\n";
+				$headers .= 'From: Forgebox.eu '. $myemail . " \r\n";
 				mail($to,$email_subject,$email_body,$headers);
 				
 				die(msg(1,"You have an email in your mail account. Please follow the instuctions!"));
